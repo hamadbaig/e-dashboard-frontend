e29a938a9f3e3777621047596d6a58a9e3326912
hamadbaig <hammad.ahmed.connect@gmail.com>
2024-04-29 10:16:14 +0500

    setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Product listing component</h1>} />
+          <Route path="/add" element={<h1>Add product component</h1>} />
+          <Route path="/update" element={<h1>Update Product component</h1>} />
+          <Route path="/delete" element={<h1>Delete Product component</h1>} />
+          <Route path="/logout" element={<h1>Logout component</h1>} />
+          <Route path="/profile" element={<h1>Profile component</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
